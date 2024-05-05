5967d7f288aa0fe8a50c645f3095770475b7b618
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2024-05-06 01:14:44 +0300

    Necessary matrix operations

--- primalnn.c
+++ primalnn.c
@@ -2,7 +2,22 @@
 #include "primalnn.h"
 
 int main(void) {
-    Matrix m = matrix_alloc(10, 10);
-    matrix_print(m);
+    Matrix a = matrix_alloc(1, 2);
+    matrix_rand(a, 0, 10);
+
+    float id_data[4] = {
+        1, 0, 
+        0, 1
+    };
+
+    Matrix b = { .rows = 2, .cols = 2, .es = id_data};
+
+    Matrix c = matrix_alloc(1, 2);
+
+    matrix_print(a);
+    printf("------------------------------\n");
+    matrix_dot(c, a, b);
+    matrix_print(c);
+
     return 0;
 }
--- primalnn.h
+++ primalnn.h
@@ -1,8 +1,9 @@
-#ifndef PRIMALNN_H_
-#define PRIMALNN_H_
-
 #include <stddef.h>
 #include <stdio.h>
+#include <time.h>
+
+#ifndef PRIMALNN_H_
+#define PRIMALNN_H_
 
 #ifndef PRIMALNN_MALLOC
 #include <stdlib.h>
@@ -22,14 +23,22 @@ typedef struct {
 
 #define MATRIX_AT(a, i, j) (a).es[(i) * (a).cols + (j)]
 
+float rand_float(void);
+
 Matrix matrix_alloc(size_t rows, size_t cols);
 void matrix_dot(Matrix c, Matrix a, Matrix b);
 void matrix_sum(Matrix c, Matrix a);
 void matrix_print(Matrix a);
+void matrix_rand(Matrix a, float low, float high);
+void matrix_fill(Matrix a, float x);
 
 #endif // PRIMALNN_H
 
 #ifdef PRIMALNN_IMPLEMENTATION
+
+float rand_float(void) {
+  return (float)rand() / (float)RAND_MAX;
+}
 
 Matrix matrix_alloc(size_t rows, size_t cols){
     Matrix m;
@@ -41,22 +50,54 @@ Matrix matrix_alloc(size_t rows, size_t cols){
 }
 
 void matrix_dot(Matrix c, Matrix a, Matrix b){
-    (void) c;
-    (void) a;
-    (void) b;
+    PRIMALNN_ASSERT(a.cols == b.rows);
+    size_t n = a.cols;
+    PRIMALNN_ASSERT(c.rows == a.rows);
+    PRIMALNN_ASSERT(c.cols == b.cols);
+
+    for (size_t i = 0; i < c.rows; i++) {
+        for (size_t j = 0; j < c.cols; j++) {
+            MATRIX_AT(c, i, j) = 0;
+            for (size_t k = 0; k < n; k++) {
+                MATRIX_AT(c, i, j) += MATRIX_AT(a, i, k) * MATRIX_AT(b, j, k);
+            }
+        }
+    }
 }
 
 void matrix_sum(Matrix c, Matrix a) {
-    (void) c;
-    (void) a;
+    PRIMALNN_ASSERT(c.rows == a.rows);
+    PRIMALNN_ASSERT(c.cols == a.cols);
+    for (size_t i = 0; i < c.rows; i++) {
+        for (size_t j = 0; j < c.cols; j++) {
+            MATRIX_AT(c, i, j) += MATRIX_AT(a, i, j);
+        }
+    }
 }
 
 void matrix_print(Matrix a) {
     for (size_t i = 0; i < a.rows; i++) {
         for (size_t j = 0; j < a.cols; j++) {
-            printf("%f", MATRIX_AT(a, i, j));
+            printf("%f ", MATRIX_AT(a, i, j));
         }
         printf("\n");
+    }
+}
+
+void matrix_rand(Matrix a, float low, float high) {
+    srand(time(0));
+    for (size_t i = 0; i < a.rows; i++) {
+        for (size_t j = 0; j < a.cols; j++) {
+            MATRIX_AT(a, i, j) = rand_float() * (high - low) + low;
+        }
+    }
+}
+
+void matrix_fill(Matrix a, float x) {
+    for (size_t i = 0; i < a.rows; i++) {
+        for (size_t j = 0; j < a.cols; j++) {
+            MATRIX_AT(a, i, j) = x;
+        }
     }
 }
 
